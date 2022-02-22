d08bc76d8252a7d0203a672aa516241d9214adc8
Will-Andrade <willian.santos.andra@outlook.com>
2022-02-22 12:31:43 -0400

    feat: creates the root variables for the project and adds the body styling

--- layout/Basic.tsx
+++ layout/Basic.tsx
@@ -8,6 +8,31 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
     }
+
+    :root {
+        --white: #FFF;
+
+        --gray-30: #A3B8B0;
+        --gray-40: #587169;
+        --gray-50: #34423D;
+
+        --shapes-fst: #EAEFED;
+        --shapes-snd: #B5F1DD;
+        --shapes-thd: #22e0A1;
+        --shapes-frt: #CAD6D1;
+
+    }
+    
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        color: #587169;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+        line-height: 1;
+    }
 `;
 
 const BasicLayout = ({ children }: { children: any }) => {
