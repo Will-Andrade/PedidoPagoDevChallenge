ea0ef0782b2462ea816cfd19847548a95f68ae03
Will-Andrade <willian.santos.andra@outlook.com>
2022-02-22 15:15:57 -0400

    feat: adds background-color  to the project

--- layout/Basic.tsx
+++ layout/Basic.tsx
@@ -28,6 +28,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
+        background: #F8FAF9;
         color: #587169;
         font-family: 'Poppins', sans-serif;
         font-weight: 400;
